6dffe16cbfa30608b1f3c71b210a1ba918dad84e
Aneeq Tariq <aneeqtariq_143@yahoo.com>
2024-10-02 17:31:48 +0500

    Added Orders Service starting message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,7 @@ import {ExpirationCompleteListener} from "./events/listeners/expiration-complete
 import {PaymentCreatedListener} from "./events/listeners/payment-created-listener";
 
 const start = async () => {
+    console.log('Starting orders service...');
     // Check if the JWT_KEY environment variable is defined
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY must be defined");
